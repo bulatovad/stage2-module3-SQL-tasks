72a8523ce83a8c581f0708dc998f038a9c687258
bulatovad <damira.bulatova@gmail.com>
2023-06-27 19:16:24 +0600

    updated solution 5

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -4,4 +4,4 @@ SELECT * FROM student WHERE groupnumber=10 AND FLOOR(DATEDIFF(dd, birthday, CURR
 SELECT * FROM student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
 SELECT * FROM payment WHERE FLOOR(DATEDIFF(dd, payment_date, CURRENT_DATE)/30) <= 8;
 SELECT * FROM student WHERE name LIKE 'A%';
-SELECT * FROM student WHERE (name='Roxi' and groupnumber = 4) OR (name='Tallie' and groupnumber=9);
+SELECT * FROM student WHERE (name like'Roxi%' and groupnumber = 4) OR (name LIKE 'Tallie%' and groupnumber=9);
